1700397dd58991732509a28baab997f7a75850f7
Raam <raam.sanghani@gmail.com>
2024-03-31 19:01:22 -0500

    added Node Class, and LinkedList Class

---
+++ script.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+}
